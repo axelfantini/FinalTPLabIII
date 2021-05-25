f218b064c6f809be0b35fa03f5ec939139965b7a
MartinMatt90 <mlmattera@gmail.com>
2021-05-25 20:38:56 -0300

    userLogic

--- src/models/Customer.java
+++ src/models/Customer.java
@@ -1,7 +1,16 @@
 package models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Customer extends User {
-    List<Booking> bookings;
+    private List<Booking> bookings = new ArrayList<>();
+
+    public Customer(String name, String dni, String country, String address, String password) {
+        super(name, dni, country, address, password);
+    }
+
+    public void addBooking (Booking booking){
+        this.bookings.add(booking);
+    }
 }
