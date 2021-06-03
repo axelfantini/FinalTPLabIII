79b93c0aac2f53e3426b751366429dbad9c8bef9
axelfantini <axel.fantini@outlook.com>
2021-06-02 21:09:02 -0300

    id to roomtype

--- src/models/RoomType.java
+++ src/models/RoomType.java
@@ -1,6 +1,8 @@
 package models;
 
-public class RoomType {
+import java.util.UUID;
+
+public class RoomType extends BaseObject<UUID> {
     private String name;
     private int capacity;
     private Double price;
