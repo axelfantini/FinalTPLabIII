7b2fb50366193bfe4575a9141b010d5db19835b8
MartinMatt90 <mlmattera@gmail.com>
2021-06-12 03:10:56 -0300

    frontValidation

--- src/controllers/ViewController.java
+++ src/controllers/ViewController.java
@@ -531,7 +531,7 @@ public class ViewController implements Initializable {
         }
     }
 
-    public void toSetup(MouseEvent mouseEvent){
+    public void toSetup(){
         try {
             Main.changeStage("/views/SetupStep1.fxml");
         } catch (IOException e) {
@@ -539,7 +539,7 @@ public class ViewController implements Initializable {
         }
     }
 
-    public void toSetupStep2(MouseEvent mouseEvent) {
+    public void toSetupStep2() {
         try {
             Main.changeStage("/views/SetupStep2.fxml");
         } catch (IOException e) {
@@ -547,7 +547,7 @@ public class ViewController implements Initializable {
         }
     }
 
-    public void toSetupStep3(MouseEvent mouseEvent) {
+    public void toSetupStep3() {
         try {
             Main.changeStage("/views/SetupStep3.fxml");
         } catch (IOException e) {
@@ -555,7 +555,7 @@ public class ViewController implements Initializable {
         }
     }
 
-    public void toSetupStep4(MouseEvent mouseEvent) {
+    public void toSetupStep4() {
         try {
             if(setupStep3TableViewData.size() > 0)
                 Main.changeStage("/views/SetupStep4.fxml");
@@ -566,7 +566,7 @@ public class ViewController implements Initializable {
         }
     }
 
-    public void toSetupStep5(MouseEvent mouseEvent) {
+    public void toSetupStep5() {
         try {
             if(setupStep4TableViewData.size() > 0)
                 Main.changeStage("/views/SetupStep5.fxml");
@@ -577,7 +577,7 @@ public class ViewController implements Initializable {
         }
     }
 
-    public void toLogin(MouseEvent mouseEvent) {
+    public void toLogin() {
         if(setupStep5TableViewData.size() > 0) {
             saveHotel();
             toLoginScene();
@@ -594,7 +594,7 @@ public class ViewController implements Initializable {
         }
     }
 
-    public void toDashboardHome(MouseEvent mouseEvent) {
+    public void toDashboardHome() {
         try {
             Main.changeStage("/views/DashboardHome.fxml");
         } catch (IOException e) {
@@ -610,7 +610,7 @@ public class ViewController implements Initializable {
         }
     }
 
-    public void toDashboardUsers(MouseEvent mouseEvent) {
+    public void toDashboardUsers() {
         try {
             Main.changeStage("/views/DashboardUsers.fxml");
         } catch (IOException e) {
@@ -618,7 +618,7 @@ public class ViewController implements Initializable {
         }
     }
 
-    public void toAdminPanel(MouseEvent mouseEvent) {
+    public void toAdminPanel() {
         try {
             Main.changeStage("/views/AdminPanel.fxml");
         } catch (IOException e) {
@@ -644,7 +644,7 @@ public class ViewController implements Initializable {
         }
     }
 
-    public void loadRoomTypes(MouseEvent mouseEvent)
+    public void loadRoomTypes()
     {
         if(setupStep5ComboType.getItems().size()  == 0)
         {
@@ -866,7 +866,7 @@ public class ViewController implements Initializable {
             {
                 if(user.getRole() != RoleEnum.USER)
                 {
-                    toDashboardHome(null);
+                    toDashboardHome();
                 }
                 else
                 {
@@ -927,7 +927,7 @@ public class ViewController implements Initializable {
         String userId = params.getValue("userId");
         ErrorResponse<User> errorResponse = Main.getActualHotel().deleteUser(userId);
         if(errorResponse.getSuccess())
-            toDashboardUsers(null);
+            toDashboardUsers();
         else
             showError("Error borrando el usuario", 1);
     }
@@ -958,7 +958,7 @@ public class ViewController implements Initializable {
         {
             Hotel hotel = new Hotel(name, address, stars);
             Main.setActualHotel(hotel);
-            toSetupStep2(null);
+            toSetupStep2();
         }
     }
 
@@ -973,7 +973,7 @@ public class ViewController implements Initializable {
             CreateUserRequest user = new CreateUserRequest(name, dni, country, address, password, RoleEnum.ADMIN);
             ErrorResponse<User> response = Main.getActualHotel().createUser(user);
             if(response.getSuccess())
-                toSetupStep3(null);
+                toSetupStep3();
             else
                 showError(response.getError().getFancyError(), 1);
         }
@@ -1525,18 +1525,22 @@ public class ViewController implements Initializable {
     }
 
     public void initializeDashboardBookings(){
+        loadTableViewDashboardBookings();
         if(Main.getActualUser().getRole() == RoleEnum.USER)
         {
+            loadBookingsToTable(Main.getActualUser().getBookings(new GetBookingRequest()));
             btnMenuDashboardHome.setDisable(true);
             btnMenuDashboardHome.setOpacity(0.75);
             btnMenuDashboardBookings.setVisible(false);
             btnMenuDashboardUsers.setVisible(false);
             btnMenuAdminPanel.setVisible(false);
         }
-        else if(Main.getActualUser().getRole() == RoleEnum.RECEPTIONIST)
+        else if(Main.getActualUser().getRole() == RoleEnum.RECEPTIONIST) {
             btnMenuAdminPanel.setVisible(false);
-        loadTableViewDashboardBookings();
-        loadBookingsToTable(Main.getActualHotel().getBookings(new GetBookingRequest()));
+            loadBookingsToTable(Main.getActualHotel().getBookings(new GetBookingRequest()));
+        }
+        else
+            loadBookingsToTable(Main.getActualHotel().getBookings(new GetBookingRequest()));
     }
 
     public void initializeDashboardHome(){
